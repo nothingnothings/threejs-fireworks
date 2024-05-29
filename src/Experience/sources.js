155259ee5abe5af9c50cafe7cecd53a5c48c7c87
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-05-29 13:28:43 -0300

    update

--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -2,46 +2,47 @@ export default [
   {
     name: 'whiteCircleTexture',
     type: 'texture',
-    path: 'particles/1.png',
+    path: '/particles/1.png',
   },
 
   {
     name: 'darkCircleTexture',
     type: 'texture',
-    path: 'particles/2.png',
+    path: '/particles/2.png',
   },
 
   {
     name: 'starLineTexture',
     type: 'texture',
-    path: 'particles/3.png',
+    path: '/particles/3.png',
   },
 
   {
     name: 'fatStarTexture',
     type: 'texture',
-    path: 'particles/4.png',
+    path: '/particles/4.png',
   },
 
   {
     name: 'starTexture',
     type: 'texture',
-    path: 'particles/5.png',
+    path: '/particles/5.png',
   },
 
   {
     name: 'brightStarTexture',
     type: 'texture',
-    path: 'particles/6.png',
+    path: '/particles/6.png',
   },
   {
     name: 'heartTexture',
     type: 'texture',
-    path: 'particles/7.png',
+    path: '/particles/7.png',
   },
   {
     name: 'cartoonStarTexture',
     type: 'texture',
-    path: 'particles/8.png',
+    path: '/particles/8.png',
   },
 ];
+
